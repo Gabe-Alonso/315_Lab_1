b54059299e0aafb49b703c6a053b2d75d6f5e462
Gabe-Alonso <gabixtreme363@outlook.com>
2024-04-11 11:53:10 -0700

    Create reverse.asm

---
+++ reverse.asm
@@ -0,0 +1,92 @@
+# declare global so programmer can see actual addresses.
+.globl welcome
+.globl prompt
+.globl sumText
+
+#  Data Area (this area contains strings to be displayed during the program)
+.data
+
+welcome:
+	.asciiz " This program adds two numbers \n\n"
+
+prompt:
+	.asciiz " Enter an integer: "
+
+sumText: 
+	.asciiz " \n New = "
+
+#Text Area (i.e. instructions)
+.text
+
+main:
+
+	# Display the welcome message (load 4 into $v0 to display)
+	ori     $v0, $0, 4			
+
+	# This generates the starting address for the welcome message.
+	# (assumes the register first contains 0).
+	lui     $a0, 0x1001
+	syscall
+
+	# Display prompt
+	ori     $v0, $0, 4			
+	
+	# This is the starting address of the prompt (notice the
+	# different address from the welcome message)
+	lui     $a0, 0x1001
+	ori     $a0, $a0,0x22
+	syscall
+
+	# Read 1st integer from the user (5 is loaded into $v0, then a syscall)
+	ori     $v0, $0, 5
+	syscall
+
+	# Clear $s0 for the sum
+	ori     $s0, $0, 0	
+
+	# Add 1st integer to sum 
+	# (could have put 1st integer into $s0 and skipped clearing it above)
+	addu    $s0, $v0, $s0
+
+	# Zero result register
+	ori     $s1, $0, 0
+
+	# Create counter for loop
+	addi     $t0, $0, 32
+	# Create bit mask to check each bit
+	addi     $t1, $0, 1
+	lui $t3, 0x8000
+
+loop:
+	and $t2, $s0, $t1
+	beq $t2, $0, bitAbs
+
+	# If bit not found to be 0, do operations
+	# Else, jump past operations
+	add $s1, $s1, $t3
+
+bitAbs:
+	
+	sll $t1, $t1, 1
+	srl $t3, $t3, 1
+	addi $t0, $t0, -1
+	beq $t0, $0, conclude
+	j loop
+
+
+conclude:
+	# Display the sum text
+	ori     $v0, $0, 4			
+	lui     $a0, 0x1001
+	ori     $a0, $a0,0x36
+	syscall
+	
+	# Display the sum
+	# load 1 into $v0 to display an integer
+	ori     $v0, $0, 1			
+	add 	$a0, $s1, $0
+	syscall
+	
+	# Exit (load 10 into $v0)
+	ori     $v0, $0, 10
+	syscall
